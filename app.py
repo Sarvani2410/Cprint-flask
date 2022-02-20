0670075d12898e7965d5fe2aacd4de5030b60dae
sarvani deekshitula <sarvanimini@gmail.com>
2022-02-19 19:47:17 -0800

    Home page

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def signup():
 def login():
     return render_template('login.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
